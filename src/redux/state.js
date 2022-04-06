a35b617016633f98b510b1b6ddbef5cdfcf4ee07
Denis <63734039+molybdenum1@users.noreply.github.com>
2022-04-06 20:54:56 +0300

    state

---
+++ src/redux/state.js
@@ -0,0 +1,19 @@
+let state = {
+    postData : [{id: 1, text: "Hi there", likes: 12 },
+                {id: 2, text: "It's my second postttt", likes: 32 },
+                {id: 3, text: "Wazzzzzzup", likes: 1 },],
+  
+    dialogsData : {
+      users : [{id: 1, name: 'Den4ik'},
+              {id: 2, name: 'Tom Holland'},
+              {id: 3, name: 'Andrew Garfield'},
+              {id: 4, name: 'Toby Maguire'}],
+  
+      messages : [{id: 1, text: 'hi there'},
+                {id: 2, text: 'wazzzzzzup'},
+                {id: 3, text: 'cooooooooo0l'},
+                {id: 4, text: 'SuuuuuuuuuuS'},
+                {id: 5, text: 'siemens'}]
+    }
+  }
+export default state
