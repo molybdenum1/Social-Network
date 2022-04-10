e917bb5f217ade41b364cd90239473a5dd64f1a1
Denis <63734039+molybdenum1@users.noreply.github.com>
2022-04-10 22:44:09 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
+import state, { subcribe } from './redux/state'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  {addPost, updateNewPostText} from './redux/state';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+
+let reRenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App 
+      data={state} 
+      addPost={addPost} 
+      updateNewPostText={updateNewPostText} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+}
+reRenderEntireTree(state)
+subcribe(reRenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
