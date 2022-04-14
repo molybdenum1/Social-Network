8d9d80c8514a1d7f30752702e159e6fe9f7085aa
Denis <63734039+molybdenum1@users.noreply.github.com>
2022-04-14 23:26:45 +0300

    actions in state

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,8 +1,11 @@
-let reRenderEntireTree = () => {
-  console.log("state is changed")
-}
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const ADD_POST = 'ADD-POST';
 
-let state = {
+const UPDATE_NEW_MESSAGE  = 'UPDATE-NEW-MESSAGE'; 
+const ADD_MESSAGE = 'ADD-MESSAGE';
+
+let store = {
+   _state : {
     postData : {
       post :    [{id: 1, text: "Hi there", likes: 12 },
                 {id: 2, text: "It's my second postttt", likes: 32 },
@@ -20,29 +23,71 @@ let state = {
                 {id: 2, text: 'wazzzzzzup'},
                 {id: 3, text: 'cooooooooo0l'},
                 {id: 4, text: 'SuuuuuuuuuuS'},
-                {id: 5, text: 'siemens'}]
+                {id: 5, text: 'siemens'}],
+      
+      newMessText : ''
+    }
+  },
+  getState() {
+    return this._state;
+  },
+
+  _callSubscriber() {
+    console.log("state is changed")
+  },
+  subcribe(observe) {
+    this._callSubscriber = observe; 
+  },
+
+
+  dispatch(action) {
+    if(action.type === 'ADD-POST'){
+        let newPost = {
+          id: 5,
+          text : this.getState().postData.newPostText,
+          likes : 1
+        }
+        this._state.postData.post.push(newPost);
+        this._callSubscriber(this._state);
+
+    } else if (action.type  === 'UPDATE-NEW-POST-TEXT'){
+        this._state.postData.newPostText = action.newText;
+        this._callSubscriber(this._state);
+
+    } else if (action.type === 'UPDATE-NEW-MESSAGE'){
+        this._state.dialogsData.newMessText = action.newText;
+        this._callSubscriber(this._state)
+
+    } else  if (action.type === 'ADD-MESSAGE'){
+        let newMess = {
+          id: 6,
+          text: this.getState().dialogsData.newMessText
+        }
+        this._state.dialogsData.messages.push(newMess);
+        this._callSubscriber(this._state);
+        
     }
   }
+}
 
-window.state = state 
-  
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    text :  state.postData.newPostText,
-    likes : 1
+export const addPostActionCreator = () => {
+  return {
+    type : ADD_POST
   }
-  state.postData.post.push(newPost);
-  reRenderEntireTree(state);
 }
 
-export const updateNewPostText = (newText) => {
-  state.postData.newPostText = newText;
-  reRenderEntireTree(state);
+export const updateNewPostTextActionCreator = (text) => {
+  return {type : UPDATE_NEW_POST_TEXT, newText: text}
 }
 
-export const subcribe = (observe) => {
-  reRenderEntireTree = observe; 
+export const addMessageActionCreator = () => {
+  return {
+    type : ADD_MESSAGE
+  }
 }
 
-export default state
+export const updateNewMessageActionCreator = (text) => {
+  return {type : UPDATE_NEW_MESSAGE, newText: text}
+}
+
+export default store;
